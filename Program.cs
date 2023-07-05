760155eeff25063e59c372ffbd03101fe8fc8bb5
duy-vuong160102 <duyvuong1601@gmail.com>
2023-07-06 00:15:34 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Tinhdientich
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine()); 
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+
+            float area = width * height;
+            Console.WriteLine("area is: " + area);
+
+        }
+    }
+}
